cf1ec975f3b99721dc3df403957ca5c19caed923
Wiktoria Banach <115671088+Wikuska@users.noreply.github.com>
2026-07-08 14:15:14 +0200

    feat: Configure CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,28 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.api.auth import router as auth_router
-from app.api.post import router as post_router
-from app.api.feed import router as feed_router
-from app.api.me import router as me_router
-from app.api.comment import post_comments_router as post_comment_router
 from app.api.comment import comments_router as comment_router
-from app.api.message import router as message_router
-from app.api.user import router as user_router
-from app.api.notification import router as notification_router
+from app.api.comment import post_comments_router as post_comment_router
+from app.api.feed import router as feed_router
 from app.api.lookup import router as lookup_router
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from app.api.me import router as me_router
+from app.api.message import router as message_router
+from app.api.notification import router as notification_router
+from app.api.post import router as post_router
+from app.api.user import router as user_router
 from app.core.exceptions import AppException
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="GuildBook API")
 
+origins = [
+    "https://guildbook-front.vercel.app",  # Public frontend
+    "http://localhost:5173",  # Local development frontend
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # Adres Twojego Reacta
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -34,6 +38,7 @@ app.include_router(feed_router)
 app.include_router(notification_router)
 app.include_router(lookup_router)
 app.include_router(me_router)
+
 
 @app.exception_handler(AppException)
 async def app_exception_handler(request: Request, exc: AppException):
